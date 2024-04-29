f8717e7d4bd148745c65bc6abf00d660559bbd9b
Amir Kidwai <findamirkidwai@gmail.com>
2024-04-29 19:00:59 +0530

    Update code_executer_test.py

--- code_executer_test.py
+++ code_executer_test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:8083/execute'
+url = 'http://127.0.0.1:8085/execute'
 files = {'file': open('/Users/amir/Desktop/code-interpreter/interpreter/lib/python3.11/site-packages/test.py', 'rb')}
 response = requests.post(url, files=files)
 print(response.json())
