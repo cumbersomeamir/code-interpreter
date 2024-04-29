a014f7f4855199ef8cb68c0a503eb620a840432c
Amir Kidwai <findamirkidwai@gmail.com>
2024-04-29 19:00:49 +0530

    Update code_executer.py

--- code_executer.py
+++ code_executer.py
@@ -28,7 +28,7 @@ def execute_python_code():
             return jsonify({"error": str(e)}), 500
             
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8083)
+    app.run(debug=True, host='0.0.0.0', port=8085)
     
 
     
